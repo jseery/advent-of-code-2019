90b43cfd4904ca6a370c288e156240979f3ef387
Jonathan Seery <seery.jonathan@gmail.com>
2019-12-09 21:55:33 -0500

    day 1 done, day 2 wip

--- puzzle-1-1.jl
+++ puzzle-1-1.jl
@@ -1,5 +1,55 @@
-println(PROGRAM_FILE); for x in ARGS; println(x); end
+using DelimitedFiles
+using Printf
 
 function get_module_fuel_requirement(module_mass)
-    floor(module_mass / 3) - 2
+    return Int(floor(module_mass / 3) - 2)
 end
+
+function test_module_fuel_requirement()
+    test_questions = [12; 14; 1969; 100756]
+    test_answers = [2; 2; 654; 33583]
+    for (q, a) in zip(test_questions, test_answers)
+        got_answer = get_module_fuel_requirement(q)
+        println("input: $q, expected output: $a, got output: $got_answer")
+        @assert get_module_fuel_requirement(q) == a
+    end
+end
+test_module_fuel_requirement()
+
+function get_fuel_fuel_requirement(module_fuel_req)
+    old_fuel = module_fuel_req
+    all_fuels = Int[module_fuel_req]
+    while true
+        new_fuel = get_module_fuel_requirement(old_fuel)
+        if new_fuel <= 0
+            break
+        end
+        push!(all_fuels, clamp(new_fuel, 0, Inf))
+        old_fuel = new_fuel
+    end
+    return all_fuels
+end
+
+function test_fuel_fuel()
+    println("testing fuel fuel requirements...")
+    test_module_mass = 100756
+    test_module_fuel_req = get_module_fuel_requirement(test_module_mass)
+    test_fuel_fuel_reqs = get_fuel_fuel_requirement(test_module_fuel_req)
+    expected_all_fuels = [33583, 11192, 3728, 1240, 411, 135, 43, 12, 2]
+    println("$test_fuel_fuel_reqs")
+    @assert test_fuel_fuel_reqs == expected_all_fuels
+    expected_sum_requirement = 50346
+    @assert sum(test_fuel_fuel_reqs) == expected_sum_requirement
+end
+test_fuel_fuel()
+
+
+data = DelimitedFiles.readdlm("data-1-1.csv", Int)
+module_fuel_reqs = map(get_module_fuel_requirement, data)
+answer_1 = sum(module_fuel_reqs)
+println("answer 1 is: $answer_1")
+
+# answer_1 is the input for part 2.
+all_fuel_reqs = map(get_fuel_fuel_requirement, module_fuel_reqs)
+answer_2 = sum(map(sum, all_fuel_reqs))
+println("answer 2: $answer_2")
---
+++ puzzle-2.jl
@@ -0,0 +1,65 @@
+using DelimitedFiles
+using Test
+using Printf
+
+part_1_data = DelimitedFiles.readdlm("data-2.txt", ',', Int)
+
+function run_program(program)
+    pos = 1
+    iteration = 0
+    while program[pos] != 99
+        iteration += 1
+        (opcode, loc1, loc2, loc3) = program[pos], program[pos+1], program[pos+2], program[pos+3]
+        if opcode == 1
+            program[loc3 + 1] = program[loc1 + 1] + program[loc2 + 1]
+        elseif opcode == 2
+            program[loc3 + 1] = program[loc1 + 1] * program[loc2 + 1]
+        else
+            error("iter $iteration, opcode: $opcode at position $pos not recognized")
+        end
+        pos += 4
+    end
+    return program
+end
+
+test_1 = [1,9,10,3,2,3,11,0,99,30,40,50]
+expect_1 = [3500,9,10,70,2,3,11,0,99,30,40,50]
+
+test_2 = [2,3,0,3,99]
+expect_2 = [2,3,0,6,99]
+
+test_3 = [2,4,4,5,99,0]
+expect_3 = [2,4,4,5,99,9801]
+
+test_4 = [1,1,1,4,99,5,6,0,99]
+expect_4 = [30,1,1,4,2,5,6,0,99]
+
+@test run_program(test_1) == expect_1
+@test run_program(test_2) == expect_2
+@test run_program(test_3) == expect_3
+@test run_program(test_4) == expect_4
+
+part_1_data[2] = 12
+part_1_data[3] = 2
+part_1_prog_result = run_program(part_1_data)
+println("$part_1_prog_result")
+
+part_2_target = 19690720
+part_2_program = DelimitedFiles.readdlm("data-2.txt", ',', Int)
+
+function run_with_noun_and_verb(program, noun, verb)
+    program[2] = noun
+    program[3] = verb
+    prog_result = run_program(program)
+    return prog_result[1]
+end
+
+for noun in 0:99
+    for verb in 0:99
+        output = run_with_noun_and_verb(part_2_program, noun, verb)
+        if output == part_2_target
+            print("output: $output - $(@sprintf("%f", 100 * noun + verb))")
+            break
+        end
+    end
+end
