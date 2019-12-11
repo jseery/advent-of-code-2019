54c2f98cc22d1eb3d6b2824e4d9543547ca7dc84
Jonathan Seery <seery.jonathan@gmail.com>
2019-12-10 22:08:09 -0500

    format

--- puzzle-2.jl
+++ puzzle-2.jl
@@ -12,11 +12,14 @@ function run_program(program)
     while program[pos] != 99
         @debug "iteration " iteration
         iteration += 1
-        (opcode, loc1, loc2, loc3) = program[pos], program[pos+1], program[pos+2], program[pos+3]
+        (opcode, loc1, loc2, loc3) = program[pos],
+            program[pos+1],
+            program[pos+2],
+            program[pos+3]
         if opcode == 1
-            program[loc3 + 1] = program[loc1 + 1] + program[loc2 + 1]
+            program[loc3+1] = program[loc1+1] + program[loc2+1]
         elseif opcode == 2
-            program[loc3 + 1] = program[loc1 + 1] * program[loc2 + 1]
+            program[loc3+1] = program[loc1+1] * program[loc2+1]
         else
             error("iter $iteration, opcode: $opcode at position $pos not recognized")
         end
@@ -25,17 +28,17 @@ function run_program(program)
     return program
 end
 
-test_1 = [1,9,10,3,2,3,11,0,99,30,40,50]
-expect_1 = [3500,9,10,70,2,3,11,0,99,30,40,50]
+test_1 = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+expect_1 = [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
 
-test_2 = [2,3,0,3,99]
-expect_2 = [2,3,0,6,99]
+test_2 = [2, 3, 0, 3, 99]
+expect_2 = [2, 3, 0, 6, 99]
 
-test_3 = [2,4,4,5,99,0]
-expect_3 = [2,4,4,5,99,9801]
+test_3 = [2, 4, 4, 5, 99, 0]
+expect_3 = [2, 4, 4, 5, 99, 9801]
 
-test_4 = [1,1,1,4,99,5,6,0,99]
-expect_4 = [30,1,1,4,2,5,6,0,99]
+test_4 = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+expect_4 = [30, 1, 1, 4, 2, 5, 6, 0, 99]
 
 @test run_program(test_1) == expect_1
 @test run_program(test_2) == expect_2
@@ -58,8 +61,8 @@ end
 
 part_2_target = 19690720
 part_2_program = DelimitedFiles.readdlm("data-2.txt", ',', Int)
-for noun in 0:99
-    for verb in 0:99
+for noun = 0:99
+    for verb = 0:99
         output = run_with_noun_and_verb(part_2_program, noun, verb)
         if output == part_2_target
             print("output: $output - $(@sprintf("%s", 100 * noun + verb))")
